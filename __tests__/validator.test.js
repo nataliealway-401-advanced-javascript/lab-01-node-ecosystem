30d72d51fce53e79e5f49bd2306d2ccb75ec7b3d
natalielyn <nlmaxwell@gmail.com>
2020-01-06 20:33:39 -0800

    added all tests, 3 pass, moved truthy statements to top of tests

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -23,8 +23,8 @@ describe('validator module performs basic validation of', () => {
   });
 
   it('numbers', () => {
-    expect(validator.isNumber(str)).toBeFalsy();
     expect(validator.isNumber(num)).toBeTruthy();
+    expect(validator.isNumber(str)).toBeFalsy();
     expect(validator.isNumber(arr)).toBeFalsy();
     expect(validator.isNumber(obj)).toBeFalsy();
     expect(validator.isNumber(func)).toBeFalsy();
@@ -32,24 +32,39 @@ describe('validator module performs basic validation of', () => {
   });
 
   it('arrays', () => {
+    expect(validator.isArray(arr)).toBeTruthy();
     expect(validator.isArray(str)).toBeFalsy();
     expect(validator.isArray(num)).toBeFalsy();
-    expect(validator.isArray(arr)).toBeTruthy();
     expect(validator.isArray(obj)).toBeFalsy();
     expect(validator.isArray(func)).toBeFalsy();
     expect(validator.isArray(bool)).toBeFalsy();
   });
 
   it('objects', () => {
-    expect(true).toBeFalsy();
+    expect(validator.isObject(obj)).toBeTruthy();
+    expect(validator.isObject(str)).toBeFalsy();
+    expect(validator.isObject(num)).toBeFalsy();
+    expect(validator.isObject(arr)).toBeFalsy();
+    expect(validator.isObject(func)).toBeFalsy();
+    expect(validator.isObject(bool)).toBeFalsy();
   });
 
   it('booleans', () => {
-    expect(true).toBeFalsy();
+    expect(validator.isBoolean(bool)).toBeTruthy();
+    expect(validator.isBoolean(obj)).toBeFalsy();
+    expect(validator.isBoolean(arr)).toBeFalsy();
+    expect(validator.isBoolean(str)).toBeFalsy();
+    expect(validator.isBoolean(num)).toBeFalsy();
+    expect(validator.isBoolean(func)).toBeFalsy();
   });
 
   it('functions', () => {
-    expect(true).toBeFalsy();
+    expect(validator.isFunction(func)).toBeTruthy();
+    expect(validator.isFunction(bool)).toBeFalsy();
+    expect(validator.isFunction(obj)).toBeFalsy();
+    expect(validator.isFunction(arr)).toBeFalsy();
+    expect(validator.isFunction(num)).toBeFalsy();
+    expect(validator.isFunction(str)).toBeFalsy();
   });
 
 });
